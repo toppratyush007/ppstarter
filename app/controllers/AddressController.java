0855d5f204b05b7335da14efa573f47865e0bd6a
Rohit Prakash <rohitprakash@gmail.com>
2018-08-08 16:45:43 +0530

    removed redundant files

--- app/controllers/AddressController.java
+++ app/controllers/AddressController.java
@@ -12,6 +12,28 @@ public class AddressController extends Controller {
      * (presented in the header as patient_id)
      * present in the header
      *
+     * We need to return only those addresses which have enabled flag set
+     * as True {@see models.db.BaseModel#enabled}
+     *
+     *
+     * The resultant JSON would look like:
+     * {
+     *     "success": true,
+     *     "addresses": [
+     *          {
+     *              "nickname": "John Doe",
+     *              "province": "Manitoba",
+     *              "postal_code": "V0S0B1",
+     *              "street_address": "410, Hasbrouck, Gali number 37, Sector 4",
+     *              "city": "Toronto",
+     *          },
+     *          {
+     *              "nickname": "Batman",
+     *              ...
+     *          },
+     *          ...
+     *     ]
+     * }
      * The addresses are given in the table
      * @see models.db.PatientAddress
      *
@@ -48,6 +70,8 @@ public class AddressController extends Controller {
 
     /**
      * @see models.db.PatientAddress entry is deleted corresponding to the {@code addressId}
+     * For deleting a particular address, we just set the
+     * enabled flag as false {@see models.db.BaseModel#enabled}
      *
      * @param addressId which we need to delete
      * @return json denoting success or failure encapsulated in
